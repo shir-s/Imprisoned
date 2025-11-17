c9dba455b5736729a05bad920a601cdf64fb22a2
David99 <ddavid3099@gmail.com>
2025-11-17 21:22:56 +0200

    clean

--- Assets/Scripts/PhysicsDrawing/IToolWear.cs
+++
@@ -1,9 +0,0 @@
-// FILEPATH: Assets/Scripts/PhysicsDrawing/IToolWear.cs
-using UnityEngine;
-
-public interface IToolWear
-{
-    // Apply wear at a world-space contact with given normal.
-    // 'amount' is in meters removed (scaled by distance you dragged).
-    void WearAt(Vector3 contactPointWorld, Vector3 surfaceNormalWorld, float amount, float radius);
-}
