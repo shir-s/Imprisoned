00e592d7c6760e675328795a372a060b4a64ce7a
David <ddavid3099@gmail.com>
2025-12-15 17:21:22 +0200

    walls

--- Assets/Scripts/JellyGame/GamePlay/NEW SCRIPTS/New Folder/IDamageable.cs
+++ Assets/Scripts/JellyGame/GamePlay/NEW SCRIPTS/New Folder/IDamageable.cs
@@ -1,8 +1,16 @@
 // FILEPATH: Assets/Scripts/Combat/IDamageable.cs
 namespace JellyGame.GamePlay.Combat
 {
+    /// <summary>
+    /// Universal interface for anything that can take damage and/or be healed.
+    /// 
+    /// IMPORTANT:
+    /// - Damage and heal are abstract units.
+    /// - The receiver decides what they mean (HP, size, armor, etc.).
+    /// </summary>
     public interface IDamageable
     {
         void ApplyDamage(float amount);
+        void Heal(float amount);
     }
 }
